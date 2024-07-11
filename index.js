6a6f421d4445a110959e4734b2b3c97bab736187
Diogo Borges <116685761+diogoborges4@users.noreply.github.com>
2024-07-11 17:35:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,12 @@ const port = process.env.PORT;
 
 const app = express();
 
-app.use(express.json());
+
+app.use(cors()); 
+
+app.use(express.json()); 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-  cors({
-    credentials: false,
-    origin: ["http://localhost:5000", "https://reactgramapp.netlify.app"],
-  })
-);
 
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
